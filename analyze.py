cd513a7ad7b174854ced25bd77f65e06e228d9b6
khersham <khersham@hotmail.com>
2015-06-28 18:56:45 +0200

    Analyze.py changed

--- analyze.py
+++ analyze.py
@@ -47,42 +47,43 @@ def query_search(db, text):
     except:
         print("Query problem. Please check the query again.")
 
-"""
-Define the necessary stopwords and stems
-"""                
-stopwords = nltk.corpus.stopwords.words('english')
-stemmer = SnowballStemmer("english")
 
-#define a tokenizer which filters out stopwords and group the words by stem
-def tokenize_and_stem(text):
+class TFIDFmember:
+    
+    #Define the necessary stopwords and stems
+    
+    def __init__(self,dataset):
+        self.token_dict = {}
+        self.stopwords = nltk.corpus.stopwords.words('english')
+        self.stemmer = SnowballStemmer("english")
+        self.vtext = self.vectorize_text(dataset)
+        self.vectorizer = TfidfVectorizer(max_df=0.8, min_df=0.2, stop_words='english',use_idf=True,tokenizer=self.tokenize_and_stem,ngram_range=(1,3))   
+        self.tfidf = self.vectorizer.fit_transform(self.token_dict.values())        
+
+    #define a tokenizer which filters out stopwords and group the words by stem
+    def tokenize_and_stem(self,text):
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
-    try:
-        tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-        # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-        filtered_tokens = [token for token in tokens if re.search('[a-zA-Z]', token)]
-        stems = [stemmer.stem(t) for t in filtered_tokens if t not in stopwords]
-        return stems
-    except:
-        print("Incorrect input for tokenization.")      
+        try:
+            tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+            # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+            filtered_tokens = [token for token in tokens if re.search('[a-zA-Z]', token)]
+            stems = [self.stemmer.stem(t) for t in filtered_tokens if t not in self.stopwords]
+            return stems
+        except:
+            print("Incorrect input for tokenization.")      
         
-#define vectorizer parameters
-#vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,min_df=0.2, stop_words='english',use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))            
-vectorizer = TfidfVectorizer(max_df=0.8, min_df=0.2, stop_words='english',use_idf=True,tokenizer=tokenize_and_stem)   
-
+        #define vectorizer parameters
+    def vectorize_text(self,texts):
+        for elem in texts:
+            if elem[1] is None:
+                textcopy = "None"
+            else:      
+                textcopy = elem[1].lower() #We convert all the texts into lower case
+            no_punctuation = textcopy.translate(None, string.punctuation) #We get rid of the punctuation
+            identifier = str(elem[0])
+            self.token_dict[identifier] = no_punctuation
+           
 exampleText = """SELECT Id, Abstract FROM Article WHERE Id BETWEEN 1415898 AND 1415927"""        
 
-token_dict = {}
-
-def vectorize_text(texts):
-    for elem in texts:
-        if elem[1] is None:
-            textcopy = "None"
-        else:      
-            textcopy = elem[1].lower() #We convert all the texts into lower case
-        no_punctuation = textcopy.translate(None, string.punctuation) #We get rid of the punctuation
-        identifier = str(elem[0])
-        token_dict[identifier] = no_punctuation
-        
-        
 def kmeanfit(number_cluster): 
-    return KMeans(n_clusters=number_cluster) #Now choose the number of clusters in K-mean    
+    return KMeans(n_clusters=number_cluster) #Now choose the number of clusters in K-mean
